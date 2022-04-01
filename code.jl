e6adce9befd6e82bda80d078ee11c28bfbc43c56
Emily Case <ecase2@wisc.edu>
2022-04-01 16:53:09 -0500

    tried and failed to read in data

---
+++ code.jl
@@ -0,0 +1,14 @@
+# emily case and katherine kwok 
+# labor econ 
+# empirical project 
+
+
+# packages 
+using CSV, DataFrames                          # load packages for handling data
+
+
+
+# read in data
+data_path = "/data/data_age4554.txt"
+
+
